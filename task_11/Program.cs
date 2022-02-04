bd951c4456002f7176df9294a061aa3640d49613
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-04 21:13:30 +0300

    add task_11

---
+++ task_11/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №18: Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y.
+
+bool X1 = false;
+bool Y1 = false;
+
+bool X2 = false;
+bool Y2 = true;
+
+bool X3 = true;
+bool Y3 = false;
+
+bool X4 = true;
+bool Y4 = true;
+
+if ( (!(X1 || Y1) == !X1 && !Y1) && (!(X2 || Y2) == !X2 && !Y2) && (!(X3 || Y3) == !X3 && !Y3) && (!(X4 || Y4) == !X4 && !Y4))
+{
+    Console.WriteLine("Истинно.");
+}
+else
+{
+    Console.WriteLine("Ложно.");
+}

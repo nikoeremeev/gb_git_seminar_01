6813553e3a9a787ff7bdb1fd57bcdbd8899bff2a
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-04 15:09:55 +0300

    add task_7

---
+++ task_7/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №14: Найти третью цифру числа или сообщить, что её нет.
+Console.Write("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+int Count = 0;
+int numbercount = num;
+while (numbercount != 0)
+{
+    numbercount /= 10;
+    Count++;
+}
+if (Count >= 3)
+{
+    int number = (num / ((int)Math.Pow(10,(-1 * (3 - Count))))) % 10;
+    Console.WriteLine("У числа " + num + " третья цифра равна " + number + ".");
+}
+else
+{
+    Console.WriteLine("У числа " + num + " НЕТ третьей цифры.");
+}

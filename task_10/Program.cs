d0160daa6b782d3028f13a23fdefe09fd12a17e7
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-04 20:52:13 +0300

    add task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №17: По двум заданным числам проверять является ли одно квадратом другого.
+
+Console.Write("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+
+if (numA * numA == numB || numB * numB == numA)
+{
+    Console.WriteLine("Является.");
+}
+else
+{
+    Console.WriteLine("Не является.");
+}

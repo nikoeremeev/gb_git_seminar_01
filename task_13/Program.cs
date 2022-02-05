2d24d828b5009476d0fc2a669422d1eee1101c87
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-05 17:43:33 +0300

    add task_13

---
+++ task_13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №20: Задать номер четверти, показать диапазоны для возможных координат.
+
+Console.Write("Введите номер четверти (от 1 до 4 включительно): ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Диапазон возможных координат для " + number + " четверти: X > 0, Y > 0.");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Диапазон возможных координат для " + number + " четверти: X < 0, Y > 0.");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Диапазон возможных координат для " + number + " четверти: X < 0, Y < 0.");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Диапазон возможных координат для " + number + " четверти: X > 0, Y < 0.");
+}
+else
+{
+    Console.WriteLine("Введите корректный номер четверти от 1 до 4 включительно!");
+}

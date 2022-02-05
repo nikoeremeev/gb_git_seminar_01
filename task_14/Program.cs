df6c2f331e30ccc7db7a0a203c7590e2042d7765
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-05 17:58:30 +0300

    add task_14

---
+++ task_14/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №21: Программа проверяет пятизначное число на палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int temporary = number;
+int reverse_number = 0;
+
+while(number != 0)
+{
+    int last_digit = number % 10;
+    reverse_number = reverse_number * 10 + last_digit;
+    number = number / 10;
+}
+
+if (reverse_number == temporary)
+{
+    Console.WriteLine("Число " + temporary + " является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Число " + temporary + " НЕ является палиндромом.");
+}

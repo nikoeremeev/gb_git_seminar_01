7df5fd89bf16b68826fdded59583cca4c1653c48
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-07 21:53:41 +0300

    change task_12

--- task_12/Program.cs
+++ task_12/Program.cs
@@ -6,7 +6,11 @@ int X = int.Parse(Console.ReadLine());
 Console.Write("Введите координату Y (не равно 0): ");
 int Y = int.Parse(Console.ReadLine());
 
-if (X > 0 && Y > 0)
+if (X == 0 || Y == 0)
+{
+    Console.WriteLine("Точка находится НИ в одной из четвертей!");
+}
+else if (X > 0 && Y > 0)
 {
     Console.WriteLine("Точка находится в первой четверти.");
 }
@@ -18,7 +22,7 @@ else if (X < 0 && Y < 0)
 {
     Console.WriteLine("Точка находится в третьей четверти.");
 }
-else
+else 
 {
     Console.WriteLine("Точка находится в четвертой четверти.");
 }

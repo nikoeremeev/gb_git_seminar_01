213374eae9b34e24acb0a7270b2ac7574616657b
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-07 22:19:13 +0300

    add task_16

---
+++ task_16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №23: Показать таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Числа" + "\t\t");
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(i + "\t");
+}
+Console.WriteLine();
+Console.Write("Квадраты" + "\t");
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(i*i + "\t");
+}

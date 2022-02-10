6b7e570b9f130ea561367d45d13bfb59fd244e76
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-10 20:59:58 +0300

    add task_20

---
+++ task_20/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №27: Определить количество цифр в числе.
+
+Console.Write("Введите число = ");
+int num = int.Parse(Console.ReadLine());
+
+int count = 0;
+if (num == 0)
+{
+    count = 1;
+}
+else
+{
+    while (num != 0)
+    {
+        count++;
+        num /= 10;
+        Console.WriteLine(num);
+    }
+}
+Console.Write("количество цифр в числе = " + count);

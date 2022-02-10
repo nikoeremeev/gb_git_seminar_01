4c07642f7776070d4454bd000d9fb450b90001f5
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-10 20:38:49 +0300

    add task_18

---
+++ task_18/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №25: Найти сумму чисел от 1 до А.
+
+Console.Write("Введите число A = ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    sum += i;
+}
+Console.Write("сумму чисел от 1 до А = " + sum);

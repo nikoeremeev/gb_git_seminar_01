053ad2293531807b90630578bbbb09a05a026411
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-10 20:43:32 +0300

    add task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №26: Возведите число А в натуральную степень B используя цикл.
+
+Console.Write("Введите число A = ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B = ");
+int B = int.Parse(Console.ReadLine());
+int product = 1;
+for (int i = 1; i <= B; i++)
+{
+    product *= A;
+}
+Console.Write("число А в натуральной степени B = " + product);

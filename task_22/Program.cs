1f9777aa11dd69cd894615798253b0c312c2c0bf
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-10 21:23:13 +0300

    add task_22

---
+++ task_22/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №29: Написать программу вычисления произведения чисел от 1 до N.
+
+
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine());
+int product = 1;
+for (int i = 1; i <= N; i++)
+{
+    product *= i;
+}
+Console.Write("произведение чисел от 1 до " + N + " = " + product);

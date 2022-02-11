b63b5b1d40a6f62a267cc3895254ce78a11f41c1
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-11 20:26:19 +0300

    add task_25

---
+++ task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №32: Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран.
+
+Console.Clear();
+int [] array = new int [8];
+Console.WriteLine();
+Console.Write("Полученный массив: [ ");
+for (int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(0,2);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine("]");

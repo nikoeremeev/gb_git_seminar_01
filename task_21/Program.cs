7c83cf9dbc235205d2bcce5c303b4e2d1851d4a0
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-11 19:54:10 +0300

    add task_21

---
+++ task_21/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №28: Подсчитать сумму цифр в числе.
+
+Console.Write("Введите число = ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (num != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (sum < 0) Console.Write("Сумма цифр в числе = " + sum * (-1));
+else Console.Write("Сумма цифр в числе = " + sum);

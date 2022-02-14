a0eb5c3b0327cb429c94d1cfeffae9f633c31e31
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-14 20:45:05 +0300

    add task_26 and task_28

---
+++ task_26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №33: Задать массив из 12 элементов, заполненных числами из [0,9]. 
+// Найти сумму положительных/отрицательных элементов массива.
+
+Console.Clear();
+int [] array = new int [12];
+int summ_p = 0;
+int summ_m = 0;
+Console.WriteLine();
+Console.Write("Полученный массив: [ ");
+for (int i = 0; i < 12; i++)
+{
+    array[i] = new Random().Next(-10,10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine("]");
+Console.WriteLine();
+for (int i = 0; i < 12; i++)
+{
+    if (array[i] > 0)
+    {
+        summ_p += array[i];
+    }
+    else
+    {
+        summ_m += array[i];
+    }
+}
+Console.WriteLine("Сумма положительных элементов массива = " + summ_p);
+Console.WriteLine("Сумма отрицательных элементов массива = " + summ_m);
---
+++ task_28/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача №35: Определить, присутствует ли в заданном массиве, некоторое число.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int [] array = new int [12];
+Console.WriteLine();
+Console.Write("Полученный массив: [ ");
+for (int i = 0; i < 10; i++)
+{
+    array[i] = new Random().Next(0,100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine("]");
+Console.WriteLine();
+int flag = 0;
+for (int i = 0; i < 10; i++)
+{
+    if (array[i] == N)
+    {
+        flag = 1;
+    }
+    
+}
+if (flag == 1)
+{
+    Console.WriteLine("Встречается.");
+}
+else
+{
+    Console.WriteLine("НЕ встречается.");
+}

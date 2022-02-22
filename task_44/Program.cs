99e64f6291ee059d7c0bf8f4e436cefa40b262e6
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-22 20:08:11 +0300

    add task_44

---
+++ task_44/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = i + j;
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}

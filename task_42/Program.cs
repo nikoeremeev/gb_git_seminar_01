e63ec557a35f7a860ea03ced47ee67c32dc31782
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-02-22 20:04:58 +0300

    add task_42

---
+++ task_42/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №49: Показать двумерный массив размером m×n, заполненный вещественными числами.
+
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+double[,] array = new double[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i,j] = Math.Round(new Random().Next(0,10) * 1.01, 2);
+        Console.Write(array[i,j] + "\t");
+    }
+    Console.WriteLine();
+}

2296f34046b3cc3d531af66df099ca6fc2d5a097
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-03-01 21:05:35 +0300

    add task_60

---
+++ task_60/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №67: Показать натуральные числа от N до 1, N задано.
+
+void ShowNumbers(int number)
+{
+    if (number < 1) return;
+    Console.Write(number + " ");
+    ShowNumbers(number - 1);
+}
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine();
+ShowNumbers(N);

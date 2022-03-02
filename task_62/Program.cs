2931ae46805f42bdebaca814a0a07032f20a070b
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-03-02 19:47:11 +0300

    add task_62

---
+++ task_62/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №69: Задайте значения M и N. Напишите программу, которая найдёт сумму элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+int summa = 0;
+int SumNumber(int m, int n)
+{
+    if(m < n)
+    {
+       summa = summa + m;
+         SumNumber(m + 1, n);
+       return summa + n;
+    }
+    else if(m > n)
+    {
+        summa = summa +n;
+        SumNumber(m,n+1);
+       return summa + m;
+    }
+    else
+    {
+       return summa;    
+    }
+}
+Console.WriteLine($"Сумма элементов в промежутке от {m} до {n} равна {SumNumber(m,n)}");

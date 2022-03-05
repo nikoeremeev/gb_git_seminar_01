bc5e38fc0235cd038fef2ac8747aaf6759043b09
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-03-05 20:10:18 +0300

    add task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №71: Написать программу вычисления функции Аккермана.
+
+int Akkerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else 
+    {
+        if (N == 0 && M > 0) return Akkerman(M - 1, 1);
+        else return Akkerman(M-1, Akkerman(M, N-1));
+    }
+
+}
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Функция Аккурмана от {M} и {N} равна {Akkerman(M, N)}");

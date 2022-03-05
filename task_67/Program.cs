9e5c4f294fe3c2f7d09eb3a0ebe36d32bac0644c
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-03-05 20:32:53 +0300

    changed task_67

--- task_67/Program.cs
+++ task_67/Program.cs
@@ -1,12 +1,19 @@
 ﻿// Задача №74: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
 // Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
 
-string[] PrintAbc(char[] abc, int N)
+void PrintAbc(char[] abc, char[] word, int N = 0)
 {
-    
+    if (N == word.Length)
+    {
+        Console.WriteLine($"{new String(word)}"); return;
+    }
+    for (int i = 0; i < abc.Length; i++)
+    {
+        word[N] = abc[i];
+        PrintAbc(abc, word, N+1);
+    }
 }
 Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine());
-Console.Write("Введите число B: ");
 char[] abc = {'а', 'и', 'с', 'в'};
-PrintAbc(abc, N);
+PrintAbc(abc, new char[N]);

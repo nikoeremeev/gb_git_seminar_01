84b062e677b418751df877de83cd034dd7f8bc87
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-12-28 19:50:38 +0300

    change README file and add task_01

---
+++ task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: 
+// По двум заданным числам проверять является ли первое квадратом второго.
+
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Число " + numberA + " является квадратом числа " +
+    numberB + ".");
+}
+else
+{
+    Console.WriteLine("Число " + numberA + " не является квадратом числа " +
+    numberB + ".");
+}

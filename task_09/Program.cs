38d33bbec03826ce23ceee78e506512e6c6add22
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-12-28 20:33:36 +0300

    change README file and add task_09

---
+++ task_09/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №9: Показать последнюю цифру трёхзначного числа.
+
+
+Console.Write("Введите трехзначное число: ");
+
+int num = int.Parse(Console.ReadLine());
+if (num > 99 && num < 1000)
+{
+    Console.WriteLine("Последняя цифра числа " + num + " равна " +
+        num % 10 + ".");
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!");
+}

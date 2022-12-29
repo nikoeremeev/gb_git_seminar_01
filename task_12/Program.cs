54ed4e63fb2b74cffa451a9c01deef9fff09ea9f
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-12-29 21:05:12 +0300

    change README file and add task_12

--- task_12/Program.cs
+++ task_12/Program.cs
@@ -1,28 +1,11 @@
-﻿// Задача №19:  Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0.
+﻿// Задача №12: Удалить вторую цифру трёхзначного числа.
 
-Console.Write("Введите координату X (не равно 0): ");
-int X = int.Parse(Console.ReadLine());
 
-Console.Write("Введите координату Y (не равно 0): ");
-int Y = int.Parse(Console.ReadLine());
+Console.Write("Введите трехзначное число: ");
 
-if (X == 0 || Y == 0)
-{
-    Console.WriteLine("Точка находится НИ в одной из четвертей!");
-}
-else if (X > 0 && Y > 0)
-{
-    Console.WriteLine("Точка находится в первой четверти.");
-}
-else if (X < 0 && Y > 0)
-{
-    Console.WriteLine("Точка находится во второй четверти.");
-}
-else if (X < 0 && Y < 0)
-{
-    Console.WriteLine("Точка находится в третьей четверти.");
-}
-else 
-{
-    Console.WriteLine("Точка находится в четвертой четверти.");
-}
+int num = int.Parse(Console.ReadLine());
+
+int number = (num / 100 * 10) + (num % 10);
+
+Console.WriteLine("Число " + num + " после удаления второй цифры " +
+    number + ".");

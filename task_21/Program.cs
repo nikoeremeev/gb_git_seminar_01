d3038c44a430adbea647886e7feb5da16871fdec
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-12-30 21:02:24 +0300

    change README and task21

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -1,13 +1,23 @@
-﻿// Задача №28: Подсчитать сумму цифр в числе.
+﻿// Задача №21: Программа проверяет пятизначное число на палиндромом.
 
-Console.Write("Введите число = ");
-int num = int.Parse(Console.ReadLine());
-int sum = 0;
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
 
-while (num != 0)
+int temporary = number;
+int reverse_number = 0;
+
+while (number != 0)
 {
-    sum += num % 10;
-    num /= 10;
+    int last_digit = number % 10;
+    reverse_number = reverse_number * 10 + last_digit;
+    number = number / 10;
 }
-if (sum < 0) Console.Write("Сумма цифр в числе = " + sum * (-1));
-else Console.Write("Сумма цифр в числе = " + sum);
+
+if (reverse_number == temporary)
+{
+    Console.WriteLine("Число " + temporary + " является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Число " + temporary + " НЕ является палиндромом.");
+}

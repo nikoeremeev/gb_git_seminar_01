4d8e6247fcb2f8f603799f912d31b04a73bb60bd
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-12-31 18:31:09 +0300

    change README and task_48

--- task_48/Program.cs
+++ task_48/Program.cs
@@ -1,27 +1,17 @@
-﻿// Задача №55: Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+﻿// Задача №48: Показать двумерный массив размером m×n заполненный целыми числами.
 
 Console.Write("Введите количество строк: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int n = int.Parse(Console.ReadLine());
 int[,] array = new int[m, n];
-Console.WriteLine();
+
 for (int i = 0; i < m; i++)
 {
     for (int j = 0; j < n; j++)
     {
         array[i, j] = new Random().Next(0, 10);
-        Console.Write(array[i, j] + "\t");
+        Console.Write(array[i, j] + " ");
     }
     Console.WriteLine();
-}
-Console.WriteLine();
-for (int j = 0; j < n; j++)
-{   
-    double average = 0;
-    for (int i = 0; i < m; i++)
-    {
-        average = average + array[i, j];
-    }
-    Console.Write($"{average / m}" + "\t");
 }

7fb5954306c16c6da2fdf3f568eb725cbac889c1
Nikolay Eremeev <95546790+nikoeremeev@users.noreply.github.com>
2022-12-31 17:15:15 +0300

    Delete task_6 directory

--- task_6/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Задача №12: Удалить вторую цифру трёхзначного числа.
-Console.Write("Введите трехзначное число: ");
-
-int num = int.Parse(Console.ReadLine());
-
-int number = (num / 100 * 10) + (num % 10);
-
-Console.WriteLine("Число " + num + " после удаления второй цифры " + number + ".");

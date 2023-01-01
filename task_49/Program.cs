46c77026ae48e9f01a5234416529d9cf6c0073fe
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-01 09:44:53 +0300

    change README and task_49

--- task_49/Program.cs
+++ task_49/Program.cs
@@ -1,32 +1,17 @@
-﻿// Задача №56: Написать программу, которая обменивает элементы первой строки и последней строки.
+﻿// Задача №49: Показать двумерный массив размером m×n, 
+// заполненный вещественными числами.
 
 Console.Write("Введите количество строк: ");
 int m = int.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int n = int.Parse(Console.ReadLine());
-int[,] array = new int[m, n];
-Console.WriteLine();
+double[,] array = new double[m, n];
+
 for (int i = 0; i < m; i++)
 {
     for (int j = 0; j < n; j++)
     {
-        array[i, j] = new Random().Next(0, 10);
-        Console.Write(array[i, j] + "\t");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-int[] temp = new int[n];
-for (int i = 0; i < n; i++)
-{
-    temp[i] = array[m-1, i];
-    array[m-1, i] = array[0, i];
-    array[0, i] = temp[i];
-}
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
+        array[i, j] = Math.Round(new Random().Next(0, 10) * 1.01, 2);
         Console.Write(array[i, j] + "\t");
     }
     Console.WriteLine();

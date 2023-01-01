39109fc41b33c156d3ca42b60597bea406ce1f01
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-01 09:46:54 +0300

    change README and task_50

--- task_50/Program.cs
+++ task_50/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача №57: Написать программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+﻿// Задача №50: В двумерном массиве n×k 
+// заменить четные элементы на противоположные.
 
 Console.Write("Введите количество строк: ");
 int m = int.Parse(Console.ReadLine());
@@ -6,44 +7,21 @@ Console.Write("Введите количество столбцов: ");
 int n = int.Parse(Console.ReadLine());
 int[,] array = new int[m, n];
 Console.WriteLine();
-Console.WriteLine("Исходная матрица:");
 for (int i = 0; i < m; i++)
 {
     for (int j = 0; j < n; j++)
     {
-        array[i, j] = new Random().Next(0, 100);
+        array[i, j] = new Random().Next(0, 10);
         Console.Write(array[i, j] + "\t");
     }
     Console.WriteLine();
 }
 Console.WriteLine();
-
 for (int i = 0; i < m; i++)
 {
-    
     for (int j = 0; j < n; j++)
     {
-        int maximum = array[i, j];
-        for (int k = j; k < n; k++)
-        {
-            if (array[i, k] > maximum)
-            {
-                maximum = array[i, k];
-                array[i,k] = array[i,j];
-                array[i,j] = maximum;
-
-            }
-        }
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-Console.WriteLine("Результирующая матрица:");
-for (int i = 0; i < m; i++)
-{
-    int minimal = array[i, 0];
-    for (int j = 0; j < n; j++)
-    {
+        if (array[i, j] % 2 == 0) array[i, j] = array[i, j] * -1;
         Console.Write(array[i, j] + "\t");
     }
     Console.WriteLine();

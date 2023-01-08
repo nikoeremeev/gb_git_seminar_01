514516d5a1fbaacb29d1a32f6ae0bb76dab79a46
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-08 19:38:17 +0300

    change README and task_60(a)

--- task_60/Program.cs
+++ task_60/Program.cs
@@ -1,69 +1,36 @@
-﻿// Задача №60: Составить частотный словарь элементов двумерного массива
+﻿/*
+Задача №60 Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том, 
+сколько раз встречается элемент входных данных.
+*/
 
 
-Console.Write("Задайте количество строк в массиве: ");
+Console.Write("Введите количество строк: ");
 int m = int.Parse(Console.ReadLine());
-Console.Write("Задайте количество столбцов в массиве: ");
+Console.Write("Введите количество столбцов: ");
 int n = int.Parse(Console.ReadLine());
-
 int[,] array = new int[m, n];
-
-void FillArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]}\t");
-        }
-        Console.WriteLine();
-    }
-}
-void SortArray(int[,] array)  //сортируем массив от максимума к минимуму
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-
-            for (int a = 0; a < array.GetLength(0); a++)
-                for (int b = 0; b < array.GetLength(1); b++)
-                    if (array[a, b] < array[i, j])
-                    {
-                        int temp = array[i, j];
-                        array[i, j] = array[a, b];
-                        array[a, b] = temp;
-                    }
-}
-
-FillArray(array);
-PrintArray(array);
-Console.WriteLine("Отсортированный массив: ");
-SortArray(array);
-PrintArray(array);
-Console.WriteLine("Частотный анализ массива: ");
-int temp = array[0, 0];
-int count = 0;
-// непосредственно сам частотный анализ
-for (int i = 0; i < array.GetLength(0); i++)
-    for (int j = 0; j < array.GetLength(1); j++)
-        if (array[i, j] == temp) count++;
-        else
-        {
-            Console.WriteLine($"Число {temp} встречается {count:00} раз, " +
-                "частота {count * 100 / array.Length:00.0} %");
-            count = 1;
-            temp = array[i, j];
-        }
-Console.WriteLine($"Число " +
-    "{array[array.GetLength(0) - 1, array.GetLength(1) - 1]} встречается " +
-    "{count:00} раз, частота {count * 100 / array.Length:00.0} %");
 Console.WriteLine();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int k = 0; k < 10; k++)
+{
+    int count = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (array[i, j] == k) count++;
+        }
+    }
+    if (count != 0) Console.WriteLine($"{k} встречается {count} раза");
+}
+

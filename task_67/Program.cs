5dbcc6e14bc9df77d642860ea00e3b8c3c17162b
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-08 19:52:34 +0300

    change README and task_67

--- task_67/Program.cs
+++ task_67/Program.cs
@@ -1,19 +1,12 @@
-﻿// Задача №74: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
-// Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+﻿// Задача №67: Показать натуральные числа от N до 1, N задано.
 
-void PrintAbc(char[] abc, char[] word, int N = 0)
+void ShowNumbers(int number)
 {
-    if (N == word.Length)
-    {
-        Console.WriteLine($"{new String(word)}"); return;
-    }
-    for (int i = 0; i < abc.Length; i++)
-    {
-        word[N] = abc[i];
-        PrintAbc(abc, word, N+1);
-    }
+    if (number < 1) return;
+    Console.Write(number + " ");
+    ShowNumbers(number - 1);
 }
 Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine());
-char[] abc = {'а', 'и', 'с', 'в'};
-PrintAbc(abc, new char[N]);
+Console.WriteLine();
+ShowNumbers(N);

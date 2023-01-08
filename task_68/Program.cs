9ca22c0188f18ea67a43761345ea11d75599abbd
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-08 19:53:24 +0300

    change README and task_68

--- task_68/Program.cs
+++ task_68/Program.cs
@@ -1,25 +1,14 @@
-﻿// Задача №75: Есть два массива info и data. Массив data состоит из нулей и единиц хранящий числа в двоичном представлении. 
-// Числа идут друг за другом без разделителей. 
-// Массив info состоит из чисел, которые представляют колличество бит чисел из массива data. 
-// Составить массив десятичных представлений чисел массива data с учётом информации из массива info.
+﻿// Задача №68: Показать натуральные числа от M до N, N и M заданы
 
-int [] data = {0, 1, 1, 1, 1, 0, 0, 0, 1 };
-int [] info = {2, 3, 3, 1 };
-int [] result = new int[info.Length];
-int count = 0;
-for (int i = 0; i < result.Length; i++)
+void ShowNumbers(int M, int N)
 {
-    int temp = 0;
-    for (int j = 0; j < info[i]; j++)
-    {
-        temp += data[count + j] * (int)Math.Pow(2, j);
-        Console.Write($"{data[count + j]}");
-    }
-    result[i] = temp;
-    Console.Write("\t");
+    if (N < M) return;
+    ShowNumbers(M, N - 1);
+    Console.Write(N + " ");
 }
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
 Console.WriteLine();
-for (int i = 0; i < result.Length; i++)
-{
-    Console.Write($"{result[i]}\t");
-}
+ShowNumbers(M, N);

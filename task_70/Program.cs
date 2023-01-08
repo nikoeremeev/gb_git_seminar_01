d571ecb73d7ed069284e15887751994a0e634cda
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-08 19:55:56 +0300

    change README and task_70

---
+++ task_70/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №70: Найти сумму цифр числа.
+
+
+int SumNumbers(int N)
+{
+    if (N == 0) return 0;
+    int last_digit = N % 10;
+    return last_digit + SumNumbers(N / 10);
+}
+Console.Write("Введите число N: ");
+int K = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Сумма цифр числа {K} равна {SumNumbers(K)}");

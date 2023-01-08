cd6a439f64a577f4afcb904d4f91a86ddfbc15ba
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-01-08 20:00:12 +0300

    change README and task_73

---
+++ task_73/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача №73:
+Написать программу показывающие первые N чисел, 
+для которых каждое следующее равно сумме двух предыдущих. 
+Первые два элемента последовательности задаются пользователем.
+*/
+
+Console.Write("Введите первое число: ");
+int numFirst = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numSecond = int.Parse(Console.ReadLine());
+Console.Write("Сколько чисел будем складывать?: ");
+int count = int.Parse(Console.ReadLine());
+
+int Summa(int a, int b, int count)
+{
+    if (count == 1) return a;
+    if (count == 2) return b;
+    else
+    {
+        int sum = Summa(a, b, count - 1) + Summa(a, b, count - 2);
+        return sum;
+    }
+}
+for (int i = 1; i <= count; i++)
+{
+    Console.Write(Summa(numFirst, numSecond, i) + "  ");
+}
+Console.WriteLine("\n");

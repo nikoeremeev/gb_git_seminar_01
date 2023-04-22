4572d03d963802a71f69b918076a37e7c1eec898
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-04-22 21:59:04 +0300

    Delete commit with: Revert "Delete task_9 directory"
    This reverts commit 6b79adf39f94718469b94e3a910b41d8668e9b33.

---
+++ task_9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №15: Дано число. Проверить кратно ли оно 7 и 23.
+
+Console.Write("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("Число " + num + " кратно 7 и 23.");
+}
+else
+{
+    Console.WriteLine("Число " + num + " НЕ кратно 7 и 23.");
+}
